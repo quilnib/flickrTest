3a6a6ea3c5f9211ec07492d80a21cb3eea263e92
Tim <timinth@gmail.com>
2015-03-31 10:40:47 -0700

    added bridging header

---
+++ FlickrTest/FlickrTest-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Foundation/Foundation.h>
+
+//for connecting to Flickr
+#import <FlickrKit/FlickrKit.h>
