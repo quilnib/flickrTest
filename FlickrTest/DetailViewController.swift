3a8c90d73742020be3ae826ba990277f8dafc1ef
Tim <timinth@gmail.com>
2015-04-01 17:40:17 -0700

    was getting bothered by incorrect aspect ratios. For most photos this is fixed

--- FlickrTest/DetailViewController.swift
+++ FlickrTest/DetailViewController.swift
@@ -25,10 +25,55 @@ class DetailViewController: UIViewController {
         self.view.addSubview(self.imageView!)
         
         if let photoDictionary = self.photo { //just in case someone forgot to set the photo variable
-            var url: NSURL = FlickrKit.sharedFlickrKit().photoURLForSize(FKPhotoSizeMedium640, fromPhotoDictionary: photoDictionary)
-            PhotoController.imageForURL(url, size: "Medium640") { (image) -> Void in
-                self.imageView!.image = image
-            }
+            FlickrKit.sharedFlickrKit().call("flickr.photos.getSizes", args: ["photo_id": photoDictionary.valueForKey("id")!], completion: { (response: [NSObject : AnyObject]!, error: NSError!) -> Void in
+                if (error != nil) {
+                    //something went wrong
+                    println("\(error.userInfo)")
+                } else {
+                    println("\(response)")
+                    
+                    var dictionary = response as NSDictionary
+                    var sizes: [AnyObject] = dictionary.valueForKeyPath("sizes.size") as [AnyObject]
+                    var height: Int?
+                    var width: Int?
+                    for( var i = 0; i < sizes.count; i++) {
+                        var temp = sizes[i] as NSDictionary
+                        if ((temp.valueForKey("label") as String) == "Medium 640") {
+                            if let tempHeight = (temp.valueForKey("height") as? String) {
+                                height = tempHeight.toInt()
+                            }
+                            if let tempWidth = (temp.valueForKey("width") as? String) {
+                                width = tempWidth.toInt()
+                            }
+                        
+                            i = sizes.count
+                        }
+                    }
+                    if (height != nil && width != nil) {
+                        if (height > width) {
+                            var x: CGFloat = CGFloat(width!) / CGFloat(2.0)
+                            var screenWidth: CGFloat = self.view.bounds.width
+                            var fromX = screenWidth - x
+                            var fromXDivTwo = fromX / 2
+                            self.imageView!.frame = CGRectMake( fromXDivTwo, -320, CGFloat(width!/2), CGFloat(height!/2))
+                        } else if (width > height) {
+                            var x: CGFloat = CGFloat(width!) / CGFloat(2.0)
+                            var screenHeight: CGFloat = self.view.bounds.height
+                            var fromX = screenHeight - x
+                            var fromXDivTwo = fromX / 2
+                            self.imageView!.frame = CGRectMake( fromXDivTwo, -320, CGFloat(width!/2), CGFloat(height!/2))
+                        } else {
+                           //no need to change it.  It's already a square
+                        }
+                    }
+                    
+                    var url: NSURL = FlickrKit.sharedFlickrKit().photoURLForSize(FKPhotoSizeMedium640, fromPhotoDictionary: photoDictionary)
+                    PhotoController.imageForURL(url, size: "Medium640") { (image) -> Void in
+                        self.imageView!.image = image
+                    }
+                }
+            })
+            
         }
         
         var tap = UITapGestureRecognizer(target: self, action: "close")
